8a232d27e0c86067332696de9a4bb311e014e2db
olyaiy <alexolyaiy@gmail.com>
2024-08-23 23:19:06 -0700

    get notes content and create ui

--- src/app/notes/[id]/page.tsx
+++ src/app/notes/[id]/page.tsx
@@ -1,11 +1,32 @@
 import React from 'react'
 
-const NotePage = () => {
-  return (
-    <div>
-      
-    </div>
-  )
+import styles from './NotePage.module.css'  // Add this line
+
+async function getNote(id: string) {
+    const res = await fetch(
+        `http://127.0.0.1:8090/api/collections/notes/records/${id}`,
+        {
+            next: { revalidate: 10 },
+        }
+    )
+    const data = await res.json();
+    return data;
 }
 
-export default page
+
+export default async function NotePage ({params}: any){
+    const note = await getNote(params.id);
+    return (
+
+        <div>
+            <h1>notes/{note.id}</h1>
+            <div className={styles.note}>
+                <h3>{note.title}</h3>
+                <h5>{note.content}</h5>
+                <p>{note.created}</p>
+            </div>
+        </div>
+    );
+}
+
+
