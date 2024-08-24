aa8e088d0ca4eabbaa8cd981080e5ddefbc7003f
olyaiy <alexolyaiy@gmail.com>
2024-08-23 23:02:25 -0700

    add notes styling

--- src/app/notes/page.tsx
+++ src/app/notes/page.tsx
@@ -1,19 +1,25 @@
+import Link from 'next/link';
+import styles from './Notes.module.css';
 import PocketBase from 'pocketbase';
+
+console.log('This log is at the top level of the file');
 
 const pb = new PocketBase('http://127.0.0.1:8090');
 
 
 // fetch a paginated records list
 async function getNotes() {
-    const resultList = await pb.collection('notes').getList(1, 50, {
-        filter: 'created >= "2022-01-01 00:00:00" && someField1 != someField2',
-    });
-
-    return resultList.items as any[];
-
-
-
+    console.log('Attempting to fetch notes...');
+    try {
+        const resultList = await pb.collection('notes').getList(1, 50);
+        console.log('API call successful:', resultList);
+        return resultList.items;
+    } catch (error) {
+        console.error('API call failed:', error);
+        return [];
+    }
 }
+
 
 export default async function NotesPage() {
     const notes = await getNotes();
@@ -32,12 +38,16 @@ export default async function NotesPage() {
   )
 }
 
-function Note({note}: {note: any}) {
-    const {id, title, content, created } = note || {};
-    return <div>
-        <h2>{note.title}</h2>
-        <h5>{note.created}</h5>
-        <p>{note.content}</p>
-    </div>
-}
-
+function Note({ note }: any) {
+    const { id, title, content, created } = note || {};
+  
+    return (
+      <Link href={`/notes/${id}`}>
+        <div className={styles.note}>
+          <h2>{title}</h2>
+          <h5>{content}</h5>
+          <p>{created}</p>
+        </div>
+      </Link>
+    );
+  }
